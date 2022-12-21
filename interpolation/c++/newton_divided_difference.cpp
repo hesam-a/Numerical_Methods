d7f093f0ee00b3afcda4cedd5736ef0cd4f1fec2
Hesam Arabzadeh <40280976+hesam-a@users.noreply.github.com>
2022-12-21 17:52:10 -0500

    Update newton_divided_difference.cpp

--- interpolation/c++/newton_divided_difference.cpp
+++ interpolation/c++/newton_divided_difference.cpp
@@ -20,6 +20,13 @@ int main(){
         std::cout << '\n';
     }
 */
+    
+    for (int i=0;i<n;i++){
+        for (int j=0;j<n+1;j++){
+            p[i][0] = x[i];  
+            p[i][1] = y[i];
+        }
+    }
 
     for (int i=2;i<n+1;i++){
         for (int j=0;j<n+1-i;j++){
